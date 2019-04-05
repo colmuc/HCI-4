87f3a4a3bec2457907288f056d386a84496f269e
Collins Mucyo <collinsmucyo@gmail.com>
2019-04-05 08:26:43 -0500

    Made Line Chart

--- src/program/View.java
+++ src/program/View.java
@@ -6,6 +6,8 @@ import javafx.animation.TranslateTransition;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
@@ -50,6 +52,7 @@ public class View extends Stage {
         buttonMove.setPrefSize(100,20);
         Label infoLabel = new Label();
         Node shape = rectangle();
+        LineChart chart = createChart();
 
         buttonInfo.setOnAction((event)->
                 infoLabel.setText("This is a desktop application that will empirically validate Fitts' Law")
@@ -72,7 +75,7 @@ public class View extends Stage {
         final Pane pane = new HBox( buttonInfo, buttonMove );
         ((HBox) pane).setSpacing(10);
         pane.setPadding( new Insets( 15, 12, 15, 12 ) );
-        VBox box = new VBox(pane, infoLabel, shape);
+        VBox box = new VBox(pane, infoLabel, shape, chart);
         box.setSpacing(10);
         return box;
     }
@@ -82,6 +85,15 @@ public class View extends Stage {
         return rectangle;
     }
 
+    private LineChart createChart(){
+        final LineChart chart = new LineChart<Number,Number>(
+                new NumberAxis( "X", 0.0, 400.0, 20.0 ),
+                new NumberAxis( "Y", 0.0, 500.0, 20.0 ) );
+        chart.setTitle( "Successful Hits" );
+        chart.setAnimated( false );
+        chart.setLegendVisible( false );
+        return chart;
+    }
 
 
 
